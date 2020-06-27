66a1d5858fc4db8ae035abb3b38e55d19077b16b
miss6muffet <65518042+miss6muffet@users.noreply.github.com>
2020-06-27 03:39:44 +0200

    location and weather
    added the js code to change the location to a wished location or current location and time. tried to add weather API but still working on it.

--- weather-app/index.js
+++ weather-app/index.js
@@ -26,3 +26,52 @@ if (minutes < 10) {
 }
 console.log(now.getMinutes);
 time.innerHTML = `${hours}:${minutes}`;
+
+///weather current location///
+function showWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.main.weather.description;
+  document.querySelector("#currenthumidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#winddirection").innerHTML =
+    response.wind.direction.code;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.wind.speed.unit
+  );
+}
+
+///API///
+function searchCity(city) {
+  let apiKey = "1f29a8457a11c97a01e380819aae6d53";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("click", handleSubmit);
+
+function showPosition(position) {
+  let apiKey = "1f29a8457a11c97a01e380819aae6d53";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let button = document.querySelector("#geolocation");
+button.addEventListener("click", getCurrentPosition);
